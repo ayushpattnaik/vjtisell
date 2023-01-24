bdd725fdc6ac322e4b246543f536981b60a78d9d
Saksham Tikoo <tikoosaksham@gmail.com>
2023-01-24 23:59:33 +0530

    setup done - saksham

--- lib/screens/chats_screen.dart
+++ lib/screens/chats_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+// only buyer can initiate a chat
+// based on the buyer and seller, we make a room
+// room[object_id][buyer_id][seller_id]
+
 chats_screen(){
    return Center(child: Text('Screen 2'),);
 
--- lib/screens/postad_screen.dart
+++ lib/screens/postad_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-postad_screen(){
-   return Center(child: Text('Screen 3'),);
+// sell -> categories -> sub categories (array)
+// title, desc, photo(select only), price
+// the moment we add, api call - put, yahin se chala jayega data to home 
+
+
+postad_screen() {
+  return Center(child: Text('Screen 3'),);
 
 }
